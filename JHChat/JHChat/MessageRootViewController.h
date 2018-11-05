2424d129df97958ef590960c9fb610bb8528de48
Gjianhao <kevinbina@163.com>
2018-11-05 16:11:32 +0800

    创建项目

---
+++ JHChat/JHChat/MessageRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHChat
+//
+//  Created by gjh on 2018/11/5.
+//  Copyright © 2018 gjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageRootViewController : UIViewController
+
+
+@end
+
---
+++ JHChat/JHChat/MessageRootViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JHChat
+//
+//  Created by gjh on 2018/11/5.
+//  Copyright © 2018 gjh. All rights reserved.
+//
+
+#import "MessageRootViewController.h"
+
+@interface MessageRootViewController ()
+
+@end
+
+@implementation MessageRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
